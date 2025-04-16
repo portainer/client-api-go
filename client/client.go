bec9753ebee03be14fbcfd25d7687b61744bc48c
deviantony <anthony.lapenna@portainer.io>
2025-04-16 21:47:44 +0000

    client: refactor proxy configuration to support k8s proxy requests

--- client/client.go
+++ client/client.go
@@ -14,18 +14,17 @@ import (
 // that uses API token authentication for all operations
 type PortainerClient struct {
 	cli      *client.PortainerClientAPI
-	proxyCfg ProxyConfiguration
+	proxyCli *ProxyClient
 }
 
-// ProxyConfiguration contains the configuration used to proxy requests via the Portainer API
-type ProxyConfiguration struct {
+// ProxyClient provides a wrapper around the http.Client that is used to proxy requests via the Portainer API
+type ProxyClient struct {
+	cli *http.Client
 	// Host is the host of the Portainer server
 	// It includes the hostname and port
-	Host string
+	host string
 	// Token is the API token used to authenticate to the Portainer server
-	Token string
-	// SkipTLSVerify is used to disable TLS verification for the proxy connection
-	SkipTLSVerify bool
+	token string
 }
 
 // ClientOption defines a functional option for configuring the Portainer client
@@ -95,10 +94,16 @@ func NewPortainerClient(host, apiKey string, opts ...ClientOption) *PortainerCli
 
 	return &PortainerClient{
 		cli: client.New(transport, nil),
-		proxyCfg: ProxyConfiguration{
-			Host:          options.host,
-			Token:         options.apiKey,
-			SkipTLSVerify: options.skipTLSVerify,
+		proxyCli: &ProxyClient{
+			cli: &http.Client{
+				Transport: &http.Transport{
+					TLSClientConfig: &tls.Config{
+						InsecureSkipVerify: options.skipTLSVerify,
+					},
+				},
+			},
+			host:  options.host,
+			token: options.apiKey,
 		},
 	}
 }
---
+++ client/proxy.go
@@ -0,0 +1,81 @@
+package client
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// ProxyRequestOptions bundles the parameters for an API (Docker, Kubernetes, etc.) proxy request.
+type ProxyRequestOptions struct {
+	// Method is the HTTP method to use (GET, POST, PUT, DELETE, etc.)
+	Method string
+	// APIPath is the API endpoint path to proxy to (e.g., "/containers/json" or "/api/v1/namespaces/default/pods"). Must include the leading slash.
+	APIPath string
+	// QueryParams is a map of query parameters to include in the request URL (can be nil)
+	QueryParams map[string]string
+	// Headers is a map of headers to include in the request (can be nil)
+	Headers map[string]string
+	// Body is the request body to send (can be nil for requests that don't have a body)
+	Body io.Reader
+}
+
+// ProxyDockerRequest proxies a request to the Docker API via the Portainer API
+// using the provided options.
+//
+// Parameters:
+//   - environmentId: The ID of the target Docker environment in Portainer
+//   - opts: Options defining the proxied request (method, path, query params, headers, body)
+//
+// Returns:
+//   - *http.Response: The response from the Docker API
+//   - error: Any error that occurred during the request
+func (c *PortainerClient) ProxyDockerRequest(environmentId int, opts ProxyRequestOptions) (*http.Response, error) {
+	baseURL := fmt.Sprintf("https://%s/api/endpoints/%d/docker%s", c.proxyCli.host, environmentId, opts.APIPath)
+	return c.proxyRequest(baseURL, opts)
+}
+
+// ProxyKubernetesRequest proxies a request to the Kubernetes API via the Portainer API
+// using the provided options.
+//
+// Parameters:
+//   - environmentId: The ID of the target Kubernetes environment in Portainer
+//   - opts: Options defining the proxied request (method, path, query params, headers, body)
+//
+// Returns:
+//   - *http.Response: The response from the Kubernetes API
+//   - error: Any error that occurred during the request
+func (c *PortainerClient) ProxyKubernetesRequest(environmentId int, opts ProxyRequestOptions) (*http.Response, error) {
+	baseURL := fmt.Sprintf("https://%s/api/endpoints/%d/kubernetes%s", c.proxyCli.host, environmentId, opts.APIPath)
+	return c.proxyRequest(baseURL, opts)
+}
+
+func (c *PortainerClient) proxyRequest(baseURL string, opts ProxyRequestOptions) (*http.Response, error) {
+	req, err := http.NewRequest(opts.Method, baseURL, opts.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create proxy request: %w", err)
+	}
+
+	// Add query parameters if provided
+	if opts.QueryParams != nil {
+		q := req.URL.Query()
+		for k, v := range opts.QueryParams {
+			q.Set(k, v)
+		}
+		req.URL.RawQuery = q.Encode()
+	}
+
+	req.Header.Set("x-api-key", c.proxyCli.token)
+
+	// Add custom headers if provided
+	for k, v := range opts.Headers {
+		req.Header.Set(k, v)
+	}
+
+	resp, err := c.proxyCli.cli.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send proxy request: %w", err)
+	}
+
+	return resp, nil
+}
